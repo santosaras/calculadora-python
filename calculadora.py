8fb2e57975fbfa137f3debff056cbe599ea26ba1
santosaras <saramcssantos@gmail.com>
2025-06-17 13:41:15 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+def somar (a, b):
+  return a + b
+
+if __name__ == "__main__":
+  print("Calculadora de soma")
+  x = int(input("Digite o primeiro número:"))
+  y = int(input("Digite o segundo número:"))
+  resultado = somar(x,y)
+  print("Resultado da soma é: {resultado})
