ac28a1983c5d4e4dd7e4b5a1fdb6fade23255575
santosaras <saramcssantos@gmail.com>
2025-06-17 14:36:36 -0300

    Adiciona a funcionalidade de subtração à calculadora.  Closes #1

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,24 @@
 def somar (a, b):
   return a + b
-
+  
+def subtrair (a,b):
+  return a - b
+  
 if __name__ == "__main__":
-  print("Calculadora de soma")
+  print("Calculadora Simples")
+  print("Escolha a operação:")
+  print("1 - Soma")
+  print("2 - Subtração")
+  operacao = input("Digite o número da operação desejada: ")
+  
   x = int(input("Digite o primeiro número:"))
   y = int(input("Digite o segundo número:"))
-  resultado = somar(x,y)
-  print("Resultado da soma é: {resultado})
+
+  if operacao == 1:
+    resultado = somar(x,y)
+    print("Resultado da soma é: {resultado}")
+  elif operacao == 2:
+    resultado = subtrair(x,y)
+    print("Resultado da subtração é: {resultado}")
+  else:
+    print("Operação inválida!")
